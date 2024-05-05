75d3302a6d1b2963ca588dc24bd30d15fd91ec03
Tutoz <tutos_aaf@hotmail.com>
2024-05-05 15:49:02 -0300

    fix

--- app.py
+++ app.py
@@ -64,7 +64,7 @@ def producto():
     for i in range(1, int(producto['cantidad_imagenes']) + 1):
         imagenes.append(f'{dir_imagenes}{i}.png')
 
-    especificaciones = load(open('static\jsons\Especificaciones.json',encoding="utf8"))
+    especificaciones = load(open('static/jsons/Especificaciones.json',encoding="utf8"))
     preguntas = consultar_preguntas(id)
 
     return render_template('producto.html', fondo="#fff", producto=producto, imgs=imagenes, especificaciones=especificaciones, preguntas=preguntas)
