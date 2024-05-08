d6978037e430fee545e3fe31b84f9af91fb4b173
Tutoz <tutos_aaf@hotmail.com>
2024-05-08 01:57:42 -0300

    script del index creado

---
+++ static/js/index.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 'auto',
+    spaceBetween: 25,
+    centerSlide: 'true',
+    loop: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
